dc0182f9368299a522e1bd34b58eba1c5f3f268c
Halid Ahmed <halidahmed809@gmail.com>
2023-05-26 17:23:17 +0300

    sayi tahmin oyunları
    C# kullanarak iki farklı örnekle sayı tahmin oyunu

---
+++ CSsayitahmin/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSsayitahmin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tahmin = 0;
+            int sayi;
+            int sayac=0;
+
+            Random rastgele = new Random();
+
+            sayi = rastgele.Next(1, 100);
+
+            while(tahmin != sayi)
+            {
+                sayac++;
+                Console.Write("Sayı Giriniz: ");
+                tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if(tahmin<sayi)
+                {
+                    Console.WriteLine("Daha Büyük Bir Sayı Giriniz.");
+                }
+                else if(tahmin>sayi)
+                {
+                    Console.WriteLine("Daha Küçük Bir Sayı Giriniz.");
+                }
+            }
+
+            Console.WriteLine("Tebrikler Bildiniz...");
+            Console.WriteLine("{0}.Denemede Buldunuz", sayac);
+
+        }
+    }
+}
---
+++ sayitahminoyunu/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace sayitahminoyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = "Sayı Tahmin Oyunu";
+            Console.Clear();
+
+            Random rastgele = new Random();
+
+            Console.Write("***** SAYI TAHMİN OYUNU *****");
+            Console.WriteLine();
+            Console.Write("0 ve 10 arasında bir sayı giriniz...");
+            Console.WriteLine();
+
+            int a, b;
+            Console.Write("Sayı Giriniz: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            b = rastgele.Next(0, 11);
+
+            if (a == b)
+            {
+                Console.WriteLine("Girdiğiniz Sayı: " + a);
+                Console.WriteLine("Şans Sayısı: " + b);
+                Console.WriteLine("Tebrikler Tahmininiz Doğru...");
+            }
+            else if(a!=b && a<=10)
+            {
+                Console.WriteLine("Girdiğiniz Sayı: " + a);
+                Console.WriteLine("Şans Sayısı: " + b);
+                Console.WriteLine("Maalesef Tahmininiz yanlış...");
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz Değer Girdiniz...");
+                Console.WriteLine("0 ve 10 arasında Değer Giriniz");
+            }
+        }
+    }
+}
